1aea17ec9a6f7337ceebef468f0acc12c9ddcbe1
Alexandr Lazyuk <Zuk8080@mail.ru>
2021-09-28 13:58:19 +0300

    Added model post

--- petnet/users/views.py
+++ petnet/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from .models import CustomUser, Post
+from django.views import View
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+__all__ = [
+    'Posts'
+]
+
+
+class Posts(View):
+    def get(self, request, user_id):
+        if request.user.is_authenticated:
+            posts = Post.objects.all()
+            user = CustomUser.objects.get(id=user_id)
+            return render(request, 'time-line.html', context={'user': user, 'posts': posts})
+        return redirect('/')
+
+    def post(self, request, user_id):
+        Post.objects.create(
+            author=CustomUser.objects.get(id=user_id),
+            text=request.POST['text']
+        )
+        return self.get(request, user_id)
