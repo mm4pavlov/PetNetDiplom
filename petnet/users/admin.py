ea16336542881be445f11eb72c4e9eddffc2d759
Nikolas <ak2108zzz@gmail.com>
2021-09-29 12:45:29 +0300

    ok

--- petnet/users/admin.py
+++ petnet/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CustomUser, Post
 
-# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'test', 'post_dt')
+
+admin.site.register(Post, PostAdmin)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('about', 'phone', 'address', 'avatar')
+
+admin.site.register(CustomUser, CustomUserAdmin)
