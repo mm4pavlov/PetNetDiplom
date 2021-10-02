381bf6072bdedba4372bc51dcbc2d85786f68b0f
Tanya <tanja9058@mail.ru>
2021-10-03 00:22:24 +0300

    Add field 'avatar'

--- petnet/users/admin.py
+++ petnet/users/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser, Post
+from .models import User
 
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'avatar']
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('author', 'test', 'post_dt')
-
-admin.site.register(Post, PostAdmin)
-
-
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('about', 'phone', 'address', 'avatar')
-
-admin.site.register(CustomUser, CustomUserAdmin)
--- petnet/users/models.py
+++ petnet/users/models.py
@@ -2,20 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class CustomUser(User):
-    about = models.CharField(max_length=500, verbose_name='About')
-    phone = models.CharField(max_length=25, verbose_name='Phone')
-    address = models.CharField(max_length=100, verbose_name='Address')
-    avatar = models.ImageField(upload_to='avatar/', null=True, blank=True)
+class User(User):
+    avatar = models.ImageField(null=True, blank=True)
 
 
-class Post(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Author')
-    text = models.TextField(max_length='1200', verbose_name='Post text')
-    post_dt = models.DateField(auto_now=True, verbose_name='Post created time')
-
-    def __str__(self):
-        return f"{self.author}'s post created at {self.post_dt}"
-
-    class Meta:
-        ordering = ('-post_dt',)
